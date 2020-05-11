6760992f23f621a0c7f43b1432b2cb539e461942
HenryKasai <henrykasai1775@gmail.com>
2020-05-11 11:26:49 -0300

    Exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Por favor, insira seu nome.");
+            string nome = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Olá, " + nome + ".");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
